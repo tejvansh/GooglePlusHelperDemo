31c84a9439989d120ec10ec6b5c21d73c7edfe1b
Tejvansh Singh Chhabra <tejvansh.ce@gmail.com>
2015-02-05 23:04:54 +0530

    Initial Commit

---
+++ GplusDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GplusDemo
+//
+//  Created by Tejvansh Singh Chhabra on 05/02/15.
+//  Copyright (c) 2015 Tejvansh Singh Chhabra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GplusDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GplusDemo
+//
+//  Created by Tejvansh Singh Chhabra on 05/02/15.
+//  Copyright (c) 2015 Tejvansh Singh Chhabra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
